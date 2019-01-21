fa2a7a99e58b9c05d6ab2eff2559e349ed93a634
VasiliiOrlov <ov_prog@mail.ru>
2019-01-21 05:39:45 +0300

    1.3. JavaDoc. Code convention[#100673]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
-package main.java.ru.job4j.calculate;
+package ru.job4j.calculate;
 
+/**
+*Class Calculate решение задачи части 001 урок1.
+*@author VasiliiOrlov
+*@since 21.01.2019
+*/
 public class Calculate
 {
+	/**
+	* Main/
+	*@param args - args.
+	*/
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
