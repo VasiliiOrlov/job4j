3e8036926c57afe9bae3e82aa005df16957f9733
VasiliiOrlov <ov_prog@mail.ru>
2019-02-06 05:12:38 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#100655]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * Class MatrixCheck.
+ * @author VasiliiOrlov.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class MatrixCheck {
+    /**
+     * Method mono - compares diagonals.
+     * @param data - array.
+     * @return - return comparison result.
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i][i] != data[i + 1][i + 1]) {
+                result = false;
+            }
+        }
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i][data.length - 1 - i] != data[i + 1][data.length - 2 - i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * test
+ * @author VasiliiOrlov
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixCheckTest {
+    /**
+     * Method whenDataMonoByTrueThenTrue - check method mono
+     */
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, true, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+    /**
+     * Method whenDataMonoByTrueThenTrue - check method mono
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
