d9bd35f5e9f504df440f88a51c52aae75da77903
VasiliiOrlov <ov_prog@mail.ru>
2019-02-07 01:49:21 +0300

    Пазлы. Тестовое задание.[#100679]

---
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -0,0 +1,105 @@
+package ru.job4j.puzzle;
+
+import ru.job4j.puzzle.firuges.Cell;
+import ru.job4j.puzzle.firuges.Figure;
+
+/**
+ * @author VasiliiOrlov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Logic {
+    private final int size;
+    private final Figure[] figures;
+    private int index = 0;
+
+    public Logic(int size) {
+        this.size = size;
+        this.figures = new Figure[size * size];
+    }
+
+    public void add(Figure figure) {
+        this.figures[this.index++] = figure;
+    }
+
+    public boolean move(Cell source, Cell dest) {
+        boolean rst = false;
+        int index = this.findBy(source);
+        if (index != -1) {
+            Cell[] steps = this.figures[index].way(source, dest);
+            if (this.isFree(steps)) {
+                rst = true;
+                this.figures[index] = this.figures[index].copy(dest);
+            }
+        }
+        return rst;
+    }
+
+    public boolean isFree(Cell ... cells) {
+        boolean result = cells.length > 0;
+        for (Cell cell : cells) {
+            if (this.findBy(cell) != -1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void clean() {
+        for (int position = 0; position != this.figures.length; position++) {
+            this.figures[position] = null;
+        }
+        this.index = 0;
+    }
+
+    private int findBy(Cell cell) {
+        int rst = -1;
+        for (int index = 0; index != this.figures.length; index++) {
+            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public boolean isWin() {
+        int[][] table = this.convert();
+        boolean result = false;
+        for (int row = 0, column = 0; row < table.length - 1 && column < table.length - 1;) {
+            if (table[row][column] == table[row][column + 1] && table[row][column] == 1) {
+                column++;
+                if (column == table.length - 2) {
+                    result = true;
+                }
+            } else {
+                row++;
+            }
+        }
+        for (int row = 0, column = 0; row < table.length - 1 && column < table.length - 1;) {
+            if (table[row][column] == table[row + 1][column] && table[row][column] == 1) {
+                row++;
+                if (row == table.length - 2) {
+                    result = true;
+                }
+            } else {
+                column++;
+            }
+        }
+        return result;
+    }
+
+    public int[][] convert() {
+        int[][] table = new int[this.size][this.size];
+        for (int row = 0; row != table.length; row++) {
+            for (int cell = 0; cell != table.length; cell++) {
+                int position = this.findBy(new Cell(row, cell));
+                if (position != -1 && this.figures[position].movable()) {
+                    table[row][cell] = 1;
+                }
+            }
+        }
+        return table;
+    }
+}
