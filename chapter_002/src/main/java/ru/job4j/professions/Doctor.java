f3af9ed9ec99e40648ce2bd7b2b11644be93d034
VasiliiOrlov <ov_prog@mail.ru>
2019-03-04 11:16:32 +0300

    1. Реализация профессий в коде[#100726]

---
+++ chapter_002/src/main/java/ru/job4j/professions/Doctor.java
@@ -0,0 +1,6 @@
+package ru.job4j.professions;
+
+public class Doctor extends Profession {
+    public Diagnose heal(Patient patient) {
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/professions/Engineer.java
@@ -0,0 +1,6 @@
+package ru.job4j.professions;
+
+public class Engineer extends Profession {
+    public Building builtAHouse (House house) {
+    }
+}
